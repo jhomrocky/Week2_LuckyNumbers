5a9b86d4100ebe6258c51be7319dd46a4c4f7359
jhomrocky <jhomrocky@gmail.com>
2017-10-06 15:13:34 -0400

    End of week, project underway

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,6 +10,49 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a low number to create a minimum range value:");
+            int lowestNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a high number to create a maximum range value:");
+            int highestNumber = int.Parse(Console.ReadLine());
+
+            int[] userNumbers = new int[6];
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Please enter 6 numbers between " + lowestNumber + " and " + highestNumber + ", each followed by pressing ENTER");
+                userNumbers[i] = int.Parse(Console.ReadLine());
+                while (userNumbers[i] < lowestNumber)
+                {
+                    Console.WriteLine("Please enter a new number between " + lowestNumber + " and " + highestNumber + ".");
+                    userNumbers[i] = int.Parse(Console.ReadLine());
+                }
+                while (userNumbers[i] > highestNumber)
+                {
+                    Console.WriteLine("Please enter a new number between " + lowestNumber + " and " + highestNumber + ".");
+                    userNumbers[i] = int.Parse(Console.ReadLine());
+                }
+
+            }
+
+            int[] jackpotNumbers = new int[5];
+
+            Random randNum = new Random();
+            for (int number = 0; number <=5 ; number++)
+            {
+                int randomNumber = randNum.Next(lowestNumber, highestNumber);
+                Console.WriteLine("{0}", randNum);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
